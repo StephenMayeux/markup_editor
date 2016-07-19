127f20f5428541cdceb94140fe9e8624ddfc34b2
Stephen Mayeux <stephenmayeux@gmail.com>
2016-07-19 10:43:08 +0900

    navigate user after creating bin

--- client/components/header.js
+++ client/components/header.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Accounts from './accounts';
-import { Link } from 'react-router';
+import { Link, browserHistory } from 'react-router';
+
 
 
 class Header extends Component {
   onBinClick(event) {
     event.preventDefault();
 
-    Meteor.call('bins.insert');
+    Meteor.call('bins.insert', (err, binId) => {
+      browserHistory.push(`/bins/${binId}`) // to programmatically navigate user to particluar URL
+    });
   }
 
   render() {
