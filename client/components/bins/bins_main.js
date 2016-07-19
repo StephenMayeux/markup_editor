e47d35f4afe49c5d1d7137f299804d7f95b07050
Stephen Mayeux <stephenmayeux@gmail.com>
2016-07-20 01:42:20 +0900

    create bin share component

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -3,6 +3,7 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Bins } from '../../../imports/collections/bins';
 import BinsEditor from './bins_editor';
 import BinsViewer from './bins_viewer';
+import BinShare from './bins_share';
 
 class BinsMain extends Component {
   render() {
@@ -13,6 +14,7 @@ class BinsMain extends Component {
       <div>
         <BinsEditor bin={this.props.bin} />
         <BinsViewer bin={this.props.bin} />
+        <BinShare bin={this.props.bin} />
       </div>
     );
   }
---
+++ client/components/bins/bins_share.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BinShare extends Component {
+  render() {
+    return (
+      <footer className="bin-share">
+        BinShare
+      </footer>
+    );
+  }
+}
+
+export default BinShare;
