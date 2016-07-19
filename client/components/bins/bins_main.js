977e690a6799b931c34c4fe8e9d0f5d42ac0357a
Stephen Mayeux <stephenmayeux@gmail.com>
2016-07-20 01:30:40 +0900

    add markdown viewer

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Bins } from '../../../imports/collections/bins';
 import BinsEditor from './bins_editor';
+import BinsViewer from './bins_viewer';
 
 class BinsMain extends Component {
   render() {
@@ -11,6 +12,7 @@ class BinsMain extends Component {
     return (
       <div>
         <BinsEditor bin={this.props.bin} />
+        <BinsViewer bin={this.props.bin} />
       </div>
     );
   }
