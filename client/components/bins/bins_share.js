bb10a91246e947206efb79532eb074478cf446a8
Stephen Mayeux <stephenmayeux@gmail.com>
2016-07-20 17:42:17 +0900

    style share bin footer

--- client/components/bins/bins_share.js
+++ client/components/bins/bins_share.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 class BinShare extends Component {
+  onShareClick() {
+    const email = this.refs.email.value;
+  }
+
   render() {
     return (
       <footer className="bin-share">
-        BinShare
+        <div className="input-group">
+          <input ref="email" className="form-control" />
+          <div className="input-group-btn">
+            <button
+              onClick={this.onShareClick.bind(this)}
+              className="btn btn-default">Share Bin</button>
+          </div>
+        </div>
       </footer>
     );
   }
