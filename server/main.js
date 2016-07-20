ab1d31b9d980c32fa9fc35b3e0c98f1baad7513f
Stephen Mayeux <stephenmayeux@gmail.com>
2016-07-21 00:46:03 +0900

    shared bins publication

--- server/main.js
+++ server/main.js
@@ -6,4 +6,16 @@ Meteor.startup(() => {
   Meteor.publish('bins', function() {
     return Bins.find({ ownerId: this.userId });
   });
+
+  Meteor.publish('sharedBins', function() {
+    const user = Meteor.users.findOne(this.userId);
+
+    if (!user) { return; }
+
+    const email = users.emails[0].address;
+
+    return Bins.find({
+      sharedWith: { $elemMatch: { $eq: email } }
+    });
+  });
 });
